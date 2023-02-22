23bcc76c70ee3679ff8f640f59d77b51857b95c6
adrianfvp <adrianf2030@gmail.com>
2023-02-22 09:05:17 -0500

    menu mobile in navbar working with js

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 const emailMenu = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu'); 
+const mobileMenuButton = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu'); 
 
 emailMenu.addEventListener('click', toggleDesktopMenu);
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive')
+}
+
+mobileMenuButton.addEventListener('click', toggleMobileMenu);
+
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle('inactive')
 }
